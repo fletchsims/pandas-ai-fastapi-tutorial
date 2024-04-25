2d89e273ab12766347eca83c599255083bac782e
Fletcher B Sims <fletcher.sims@gmail.com>
2024-04-25 08:05:21 -0400

    add in main classes

---
+++ app/core/llm/openai.py
@@ -0,0 +1,39 @@
+from pandasai import SmartDataframe
+from pandasai.llm import OpenAI
+
+from app.core.parser.response_parser import PandasDataFrame
+
+
+class OpenAI(OpenAI):
+
+    def __init__(
+            self,
+            api_token,
+            model: str = "gpt-4",
+            name: str = None,
+            description: str = None,
+            config=None,
+            **kwargs
+    ):
+        super().__init__(api_token, **kwargs)
+        self.llm = OpenAI(api_token=api_token, model=model)
+
+        if name is None:
+            name = "DataFrame"
+
+        if description is None:
+            description = "Rows of data"
+
+        if config is None:
+            config = {"enforce_privacy": False,
+                      "llm": self.llm, "verbose": True,
+                      "response_parser": PandasDataFrame
+                      }
+            
+        self.name = name
+        self.description = description
+        self.config = config
+
+    def chat(self, df, prompt, config):
+        df = SmartDataframe(df, name=self.name, description=self.description, config=config)
+        return df.chat(prompt)
---
+++ app/core/parser/response_parser.py
@@ -0,0 +1,19 @@
+import json
+
+import pandas as pd
+from fastapi.responses import JSONResponse
+from pandasai.responses.response_parser import ResponseParser
+
+class PandasDataFrame(ResponseParser):
+
+    def __init__(self, context) -> None:
+        super().__init__(context)
+
+
+    def parse(self, result):
+        res_is_str_and_has_error = (isinstance(result["value"], str)) and ("invalid" in result["value"].lower() or "openai" in result["value"].lower() or "error" in result["value"].lower())
+        error_msg = "Sorry, I am having trouble answering that question. Please try again."
+        df = pd.DataFrame([result]).to_json(lines=True, orient='records')
+        main_msg = json.loads(df)
+
+        return JSONResponse(error_msg) if res_is_str_and_has_error else JSONResponse(main_msg)
